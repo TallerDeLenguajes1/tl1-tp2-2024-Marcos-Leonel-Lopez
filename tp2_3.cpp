3fbacefe48026948686f6e455415f9465fd2d85c
Marcos Lopez <marcosleonellopez@gmail.com>
2024-04-09 15:38:39 -0300

    Ej3 completo

---
+++ tp2_3.cpp
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include <math.h>
+
+#define N 5
+#define M 7
+
+int main()
+{
+    int i, j;
+    int mt[N][M];
+    for (i = 0; i < N; i++)
+    {
+        for (j = 0; j < M; j++)
+        {
+            mt[i][j] =  1 + rand() % 100;
+            printf("|\t%d\t|", mt[i][j]);
+        }
+        printf("\n");
+    }
+    return 0;
+}
